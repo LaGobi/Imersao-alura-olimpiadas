5c18b80446e57f468645b751e0627928c8928cac
Laura Gobi <lauragoby2016@gmail.com>
2024-09-09 23:44:08 -0300

    Imersao

---
+++ Imersão/app.js
@@ -0,0 +1,47 @@
+function pesquisar() {
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    let campoPesquisa = document.getElementById ("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado, Você precisa digitar o nome de um atleta ou esporte.</p>" 
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+        titulo = dado.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        //se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes (campoPesquisa)) {
+            //cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+              <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+              </h2>
+              <p class="descricao-meta">${dado.descricao}</p>
+              <a href=${dado.link} target="_blank">Mais Informações</a>
+            </div>
+        `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p> Nada foi encontado </p>"
+    }
+  
+    // Atribui o HTML gerado para o conteúdo da seção
+    section.innerHTML = resultados;
+  }
---
+++ Imersão/dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo:"Rayssa Leal",
+        descricao:"Rayssa Leal é uma skatista profissional brasileira, conhecida como a 'Fada do Skate'. Ela se tornou a mais jovem medalhista olímpica da história do Brasil.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags:"skate fada sk8"
+    },
+    {
+        titulo:"Beatriz Souza",
+        descricao:"Beatriz Souza é uma judoca brasileira, conhecida por sua técnica e força. Ela conquistou diversas medalhas em campeonatos nacionais e internacionais.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags:"judô judo judoka"   
+    },
+    {
+        titulo:"Rebeca Andrade",
+        descricao:"Rebeca Andrade é a maior ginasta brasileira de todos os tempos! Ela conquistou várias medalhas de ouro em competições internacionais, como os Jogos Olímpicos.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags:"ginástica ginastica ouro"
+    
+    }
+];
+
