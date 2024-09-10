104cafe5e8ce9a428a991a85421cbe7422012472
Laura Gobi <lauragoby2016@gmail.com>
2024-09-10 16:46:52 -0300

    atualização

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function pesquisar() {
     let section = document.getElementById("resultados-pesquisa");
   
     let campoPesquisa = document.getElementById ("campo-pesquisa").value
-
+  
     // se campoPesquisa for uma string sem nada
     if (!campoPesquisa) {
         section.innerHTML = "<p>Nada foi encontrado, Você precisa digitar o nome de um atleta ou esporte.</p>" 
@@ -20,7 +20,7 @@ function pesquisar() {
 
     // Itera sobre cada dado na lista de dados
     for (let dado of dados) {
-        titulo = dado.toLowerCase()
+        titulo = dado.titulo.toLowerCase()
         descricao = dado.descricao.toLowerCase()
         tags = dado.tags.toLowerCase()
         //se titulo includes campoPesquisa
